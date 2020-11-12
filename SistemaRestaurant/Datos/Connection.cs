9cdabfc280f2fcaba21770331c9f48e78bcf3750
Maikol Soro <maikolsoro.z1998@gmail.com>
2020-11-11 20:22:12 -0600

    The connection was created to connect the encryption and decryption database

---
+++ SistemaRestaurant/Datos/Connection.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Data;
+namespace SistemaRestaurant.Datos
+{
+    class Connection
+    {
+
+        public static string connection = Convert.ToString(Logica.Decryption.checkServer());
+        public static SqlConnection connect = new SqlConnection(connection);
+
+        public static void open()
+        {
+
+            if (connect.State == ConnectionState.Closed)
+            {
+                connect.Open();
+            }
+        }
+
+        public static void cerrar()
+        {
+            if(connect.State == ConnectionState.Open)
+            {
+                connect.Close();
+            }
+        }
+    }
+}
---
+++ SistemaRestaurant/Logica/Decryption.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Data.SqlClient;
+using System.Xml;
+
+namespace SistemaRestaurant.Logica
+{
+    class Decryption
+    {
+
+        static private AES aes = new AES();
+        static public string CnString;
+        private static string dbcnString;
+        public static string appPwdUnique = "softwareRestaurant_en_c#_SQLServer.";
+
+
+        /// <summary>
+        /// Desencryptar la conexion de la BD
+        /// </summary>
+        /// <returns></returns>
+        public static object checkServer()
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load("ConnectionString.xml");
+            XmlElement root = doc.DocumentElement;
+            dbcnString = root.Attributes.Item(0).Value;
+            CnString = (aes.Decrypt(dbcnString, appPwdUnique, int.Parse("256")));
+            return CnString;
+
+        }
+        public static object checkServerWEB()
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load("Conexion_web.xml");
+            XmlElement root = doc.DocumentElement;
+            dbcnString = root.Attributes.Item(0).Value;
+            CnString = (aes.Decrypt(dbcnString, appPwdUnique, int.Parse("256")));
+            return CnString;
+
+        }
+    }
+}
