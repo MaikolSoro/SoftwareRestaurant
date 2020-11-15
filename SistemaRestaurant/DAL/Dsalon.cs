b60f5cd21514d827c3c768472a93c1f5a9135335
Maikol Soro <maikolsoro.z1998@gmail.com>
2020-11-15 11:55:50 -0600

    The logic was created to insert a room

---
+++ SistemaRestaurant/DAL/Dsalon.cs
@@ -0,0 +1,37 @@
+﻿using SistemaRestaurant.BOL;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SistemaRestaurant.DAL
+{
+   public class Dsalon
+    {
+        public bool insert_Salon(Lsalon parameters)
+        {
+            try
+            {
+                Connection.open();
+                SqlCommand cmd = new SqlCommand("insert_salon", Connection.connect);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@Salon", parameters.Salon);
+                cmd.ExecuteNonQuery();
+                return true;
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return false;
+            }
+            finally
+            {
+                Connection.close();
+
+            }
+        }
+    }
+}
