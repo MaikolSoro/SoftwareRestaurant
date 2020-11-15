da36137a953f8c834a119b109fcbd97c7a5b56b3
Maikol Soro <maikolsoro.z1998@gmail.com>
2020-11-15 12:20:27 -0600

    The form Salons design was created

---
+++ SistemaRestaurant/GUI/Tables_salons/Salons.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using SistemaRestaurant.DAL;
+namespace SistemaRestaurant.GUI.Tables_salons
+{
+    public partial class Salons : Form
+    {
+        public Salons()
+        {
+            InitializeComponent();
+        }
+
+        private void Salones_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+        }
+       
+    }
+}
