ed6a472869b18ab7e7613b4813f6b48d994d3740
Maikol Soro <maikolsoro.z1998@gmail.com>
2020-11-16 11:15:05 -0600

    Products design was created

---
+++ SistemaRestaurant/GUI/Products/Productos_rest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.IO;
+using SistemaRestaurant.DAL;
+namespace SistemaRestaurant.GUI.Products
+{
+    public partial class Productos_rest : Form
+    {
+        public Productos_rest()
+        {
+            InitializeComponent();
+        }
+        public static int id_grupo;
+
+        private void Productos_rest_Load(object sender, EventArgs e)
+        {
+        }
+      
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+        }
+        public void frmGrupos_FormClosed(Object sender, FormClosedEventArgs e)
+        {
+        }
+
+        private void toolStripMenuItem3_Click(object sender, EventArgs e)
+        {
+           
+        }
+        public void frmRegistroProducto_FormClosed(Object sender, FormClosedEventArgs e)
+        {
+        }
+    }
+
+}
