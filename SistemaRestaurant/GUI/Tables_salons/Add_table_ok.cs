af28e266ef4d36b856bac47a2873fc6d27682246
Maikol Soro <maikolsoro.z1998@gmail.com>
2020-11-16 12:48:09 -0600

    Design add Tables

---
+++ SistemaRestaurant/GUI/Tables_salons/Add_table_ok.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using SistemaRestaurant.DAL;
+namespace SistemaRestaurant.GUI.Tables_salons
+{
+    public partial class Add_table_ok : Form
+    {
+        public Add_table_ok()
+        {
+            InitializeComponent();
+        }
+
+        private void Agregar_mesa_ok_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+        }
+       
+
+        private void Button7_Click(object sender, EventArgs e)
+        {
+        }
+    }
+}
