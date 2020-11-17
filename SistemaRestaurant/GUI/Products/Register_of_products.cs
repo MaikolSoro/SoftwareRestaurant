760fc2422aa32f8323c8492700dc97a8a9c3e66b
Maikol Soro <maikolsoro.z1998@gmail.com>
2020-11-17 10:49:38 -0600

    The Products Registration form was created.

---
+++ SistemaRestaurant/GUI/Products/Register_of_products.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+namespace SistemaRestaurant.GUI.Products
+{
+    public partial class Register_of_products : Form
+    {
+        public Register_of_products()
+        {
+            InitializeComponent();
+        }
+        private void Button1_Click(object sender, EventArgs e)
+        {
+           
+        }
+        
+
+        private void Registro_de_productos_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+        }
+       
+
+        private void PictureBox2_Click(object sender, EventArgs e)
+        {
+        }
+    }
+}
