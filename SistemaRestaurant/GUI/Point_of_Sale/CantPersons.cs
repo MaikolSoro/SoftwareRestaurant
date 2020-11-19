f0350122dd70beaef85767f9d50edc70f242e987
Maikol Soro <maikolsoro.z1998@gmail.com>
2020-11-19 12:06:36 -0600

    Design of the number of people form

---
+++ SistemaRestaurant/GUI/Point_of_Sale/CantPersons.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SistemaRestaurant.GUI.Point_of_Sale
+{
+    public partial class CantPersons : Form
+    {
+        public CantPersons()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn0_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnborrar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnborrarderecha_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void btnAceptar_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void CantPersonas_Load(object sender, EventArgs e)
+        {
+        }
+    }
+}
